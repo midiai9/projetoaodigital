ec7022b2443886423be577433080ea7147313ec2
Marcelo i9 <marcelo@i9criacoes.com.br>
2026-06-19 16:05:34 -0300

    test: marcador para validar deploy automatico

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
         <h1>Índice Nacional de Vazamento de Valor</h1>
         <p>Estamos construindo esta página. Em breve, no ar.</p>
         <code>projeto.aodigital.net.br</code>
+        <span data-build="deploy-test-1" hidden />
       </div>
     </main>
   )
